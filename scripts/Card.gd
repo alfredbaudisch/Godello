9d8c129f4c759a3ad27dcc4c6bfea36cb896afe0
Alfred Reinold Baudisch <alfred.r.baudisch@gmail.com>
2020-05-04 11:53:13 +0200

    Card elements transformed in placeholder when dragged

--- scripts/Card.gd
+++ scripts/Card.gd
@@ -3,6 +3,11 @@ extends MarginContainer
 var model : CardModel setget set_model, get_model
 var is_dragged := false setget set_is_dragged
 
+onready var style_default := preload("res://assets/style_panel_card.tres")
+onready var style_dragged := preload("res://assets/style_panel_card_dragged.tres")
+
+onready var content_container := $CardContent
+onready var content_padding_container := $CardContent/InnerPadding
 onready var title_label := $CardContent/InnerPadding/HBoxContainer/Title
 
 func set_model(_model : CardModel):
@@ -13,5 +18,17 @@ func get_model():
 	return model
 
 func set_is_dragged(value := true):
-	is_dragged = value	
-	set_visible(not value)
+	if value:
+		content_container.set("custom_styles/panel", style_dragged)		
+		title_label.set_visible_characters(0)		
+		set("mouse_filter", MOUSE_FILTER_PASS)
+		content_container.set("mouse_filter", MOUSE_FILTER_PASS)
+		content_padding_container.set("mouse_filter", MOUSE_FILTER_PASS)		
+	else:
+		content_container.set("custom_styles/panel", style_default)		
+		title_label.set_visible_characters(-1)		
+		set("mouse_filter", MOUSE_FILTER_STOP)
+		content_container.set("mouse_filter", MOUSE_FILTER_STOP)
+		content_padding_container.set("mouse_filter", MOUSE_FILTER_STOP)
+		
+	is_dragged = value
--- scripts/CardDragger.gd
+++ scripts/CardDragger.gd
@@ -1,23 +1,23 @@
 extends MarginContainer
 
-onready var panel = $"."
-onready var edit_icon = $HBoxContainer/EditIcon
-onready var title_label = $HBoxContainer/Title
-onready var split = $HBoxContainer/Split
+onready var edit_icon := $HBoxContainer/EditIcon
+onready var title_label := $HBoxContainer/Title
+onready var split := $HBoxContainer/Split
 
-var card_scene = preload("res://scenes/CardPreview.tscn")
+onready var card_owner := get_parent().get_parent()
+onready var card_scene := preload("res://scenes/CardPreview.tscn")
 
 func _ready():
 	split.set_visible(true)
 	edit_icon.set_visible(false)
 
 func _on_Card_mouse_entered():
-	edit_icon.set_visible(true)
-	split.set_visible(false)
+	if not card_owner.is_dragged:
+		_invert_edit_icon_visibility()
 
 func _on_Card_mouse_exited():
-	edit_icon.set_visible(false)
-	split.set_visible(true)
+	if not card_owner.is_dragged:
+		_invert_edit_icon_visibility()
 
 func get_drag_data(_pos):
 	var owner_card_element = get_parent().get_parent()
@@ -27,6 +27,12 @@ func get_drag_data(_pos):
 	card.set_data(owner_card_element.get_model())
 	get_parent().remove_child(card)
 	set_drag_preview(card)
+	
 	owner_card_element.set_is_dragged()
+	_invert_edit_icon_visibility()
 	
 	return card
+
+func _invert_edit_icon_visibility():
+	edit_icon.set_visible(not edit_icon.is_visible())
+	split.set_visible(not split.is_visible())
