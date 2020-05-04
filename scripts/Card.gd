a9a4e8a01458bea276824f78726852abe893098c
Alfred Reinold Baudisch <alfred.r.baudisch@gmail.com>
2020-05-04 12:12:59 +0200

    Card dragging logic all inside Card.gd, instead of a separate script

--- scripts/Card.gd
+++ scripts/Card.gd
@@ -9,6 +9,15 @@ onready var style_dragged := preload("res://assets/style_panel_card_dragged.tres
 onready var content_container := $CardContent
 onready var content_padding_container := $CardContent/InnerPadding
 onready var title_label := $CardContent/InnerPadding/HBoxContainer/Title
+onready var edit_icon := $CardContent/InnerPadding/HBoxContainer/EditIcon
+onready var split := $CardContent/InnerPadding/HBoxContainer/Split
+
+onready var card_owner := get_parent().get_parent()
+onready var card_drag_preview := preload("res://scenes/CardPreview.tscn")
+
+func _ready():
+	split.set_visible(true)
+	edit_icon.set_visible(false)
 
 func set_model(_model : CardModel):
 	model = _model
@@ -32,3 +41,27 @@ func set_is_dragged(value := true):
 		content_padding_container.set("mouse_filter", MOUSE_FILTER_STOP)
 		
 	is_dragged = value
+
+func get_drag_data(_pos):	
+	var card = card_drag_preview.instance()
+	get_parent().add_child(card)
+	card.set_data(get_model())
+	get_parent().remove_child(card)
+	set_drag_preview(card)
+	
+	set_is_dragged()
+	_invert_edit_icon_visibility()
+	
+	return card
+
+func _on_Card_mouse_entered():
+	if not is_dragged:
+		_invert_edit_icon_visibility()
+
+func _on_Card_mouse_exited():
+	if not is_dragged:
+		_invert_edit_icon_visibility()
+		
+func _invert_edit_icon_visibility():
+	edit_icon.set_visible(not edit_icon.is_visible())
+	split.set_visible(not split.is_visible())
