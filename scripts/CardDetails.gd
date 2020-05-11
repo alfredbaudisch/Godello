4289776d8ea3ba9276406b014a5630d7cdd14f29
Alfred Reinold Baudisch <alfred.r.baudisch@gmail.com>
2020-05-11 21:56:54 +0200

    Better handling of error; initial handling of checklist create and edit

--- scripts/CardDetails.gd
+++ scripts/CardDetails.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var can_close := true
+var is_save_title_manually_requested := false
 
 onready var title_label := $ScrollContainer/PanelContainer/MarginContainer/VBoxContainer/TitleRow/TitleContainer/Title
 onready var title_edit := $ScrollContainer/PanelContainer/MarginContainer/VBoxContainer/TitleRow/TitleContainer/TitleEdit
@@ -22,13 +23,8 @@ onready var close_button := $ScrollContainer/PanelContainer/MarginContainer/VBox
 var card : CardModel setget set_card
 var list : ListModel setget set_list
 
-signal close_details_requested
-
 func _ready():
 	DataRepository.connect("card_updated", self, "_on_card_updated")
-
-func open(_model : CardModel):
-	set_card(_model)	
 	title_edit.set_visible(false)
 	title_label.set_visible(true)	
 
@@ -46,7 +42,7 @@ func set_list(_model : ListModel):
 	subtitle_label.set_bbcode("in list [u]" + list.title + "[/u]")
 
 func _on_CloseButton_pressed():
-	emit_signal("close_details_requested")
+	queue_free()
 
 func _sync_tasks():
 	_reset_checkitem_edit_container()
@@ -55,40 +51,45 @@ func _sync_tasks():
 		checklist_items_container.remove_child(child)
 		child.queue_free()
 		
-	if card.tasks.size() > 0:
-		checklist_row.set_visible(true)
+	checklist_row.set_visible(true)
 		
+	if card.tasks.size() > 0:
 		for task in card.tasks:
 			var checkitem = CHECKITEM_SCENE.instance()
 			checklist_items_container.add_child(checkitem)
 			checkitem.set_model(task)			
 			checkitem.connect("edit_check_item_requested", self, "_on_edit_check_item_requested")
-			
-	else:
-		checklist_row.set_visible(false)
 		
-func _on_TitleEdit_focus_exited():
-	_save_card_title()
-
-func _on_Title_gui_input(event):
-	if event is InputEventMouseButton and event.get_button_index() == BUTTON_LEFT and not event.is_pressed():
-		can_close = false
-		title_edit.set_text(card.title)
-		title_edit.set_visible(true)
-		title_label.set_visible(false)
-		title_edit.grab_focus()
-
 func _on_SaveDescriptionButton_pressed():
 	card.set_description(description_edit.get_text())
 	
 func _input(event):
 	if Input.is_action_just_released("ui_cancel") and can_close:
-		emit_signal("close_details_requested")
+		queue_free()
+		_close_edit_card_title()
+
+func _on_card_updated(_card):
+	if card and _card.id == card.id:
+		set_card(_card)
+
+func _create_single_error_popup(message : String, focus_after_close : Control):
+	var popup = POPUP_SCENE.instance()
+	popup.get_node("Label").set_text(message)
+	add_child(popup)
+	popup.popup_centered()
+	
+	yield(popup, "tree_exited")
+	focus_after_close.grab_focus()
+	
+#
+# Title
+#
 
 func _on_TitleEdit_gui_input(event):
 	if event is InputEventKey and not event.is_pressed():
 		match event.get_scancode():
 			KEY_ENTER:
+				is_save_title_manually_requested = true
 				_save_card_title()
 			
 			# Cancel changes
@@ -100,30 +101,71 @@ func _close_edit_card_title():
 	title_edit.set_visible(false)
 	title_label.set_visible(true)
 	can_close = true
+	is_save_title_manually_requested = false
 	
 func _save_card_title():
-	if card.title == title_edit.get_text():
+	var title = title_edit.get_text().replace("\n", "").replace("\t", "").trim_suffix(" ").trim_prefix(" ")
+	
+	if title != "" and card.title == title:
 		_close_edit_card_title()
 		return
 	
-	var title = title_edit.get_text().replace("\n", "")
 	if title == "":
-		var popup = POPUP_SCENE.instance()
-		popup.get_node("Label").set_text("Title is required.")
-		add_child(popup)
-		popup.popup_centered()
-		
+		_create_single_error_popup("Title is required.", title_edit)
+		is_save_title_manually_requested = false
 	else:
 		card.set_title(title)
 		_close_edit_card_title()
 
-func _on_SaveCheckItemButton_pressed():
-	assert(checkitem_edit.get_text() != "")
-	
-	var result = DataRepository.create_task(card, checkitem_edit.get_text())
+func _on_TitleEdit_focus_exited():
+	if not is_save_title_manually_requested:
+		_save_card_title()
+
+func _on_Title_gui_input(event):
+	if event is InputEventMouseButton and event.get_button_index() == BUTTON_LEFT and not event.is_pressed():
+		can_close = false
+		title_edit.set_text(card.title)
+		title_edit.set_visible(true)
+		title_label.set_visible(false)
+		title_edit.grab_focus()
+
+#
+# Checkitem / Task
+#
+
+func _on_CheckItemEdit_gui_input(event):
+	if event is InputEventKey and not event.is_pressed():
+		match event.get_scancode():
+			KEY_ENTER:
+				_save_checkitem_task()
+
+			KEY_ESCAPE:
+				_cancel_checkitem_task_edit()
+				
+func _reset_checkitem_edit_container():
 	checkitem_edit.set_text("")
-	card = result["card"]
-	_sync_tasks()
+	checkitem_edit_container.get_parent().remove_child(checkitem_edit_container)
+	checklist_content.add_child(checkitem_edit_container)
+	
+func _on_SaveCheckItemButton_pressed():	
+	_save_checkitem_task()
+		
+func _save_checkitem_task():
+	var title = checkitem_edit.get_text().replace("\n", "").trim_suffix(" ").trim_prefix(" ")
+	
+	if title == "":
+		_create_single_error_popup("Task description is required.", checkitem_edit)
+	else:
+		var result = DataRepository.create_task(card, title)
+		checkitem_edit.set_text("")
+		card = result["card"]
+		_sync_tasks()	
+	
+func _on_DeleteCheckItemButton_pressed():
+	pass
+	
+func _cancel_checkitem_task_edit():
+	pass
 
 func _on_edit_check_item_requested(_node):	
 	if checkitem_edit_container.get_parent() == checklist_content:
@@ -132,12 +174,3 @@ func _on_edit_check_item_requested(_node):
 	checklist_items_container.add_child_below_node(_node, checkitem_edit_container)
 	checklist_items_container.move_child(_node, checkitem_edit_container.get_index() - 1)
 	checkitem_edit.set_text(_node.model.title)
-
-func _on_card_updated(_card):
-	if card and _card.id == card.id:
-		set_card(_card)
-
-func _reset_checkitem_edit_container():
-	checkitem_edit.set_text("")
-	checkitem_edit_container.get_parent().remove_child(checkitem_edit_container)
-	checklist_content.add_child(checkitem_edit_container)
