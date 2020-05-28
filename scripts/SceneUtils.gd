02000e7c9c070d902abd2d49696d7aefde9e90e3
Alfred Reinold Baudisch <alfred.r.baudisch@gmail.com>
2020-05-28 21:28:29 +0200

    Edit list and board dialog can be created from SceneUtils now #2 #3

--- scripts/SceneUtils.gd
+++ scripts/SceneUtils.gd
@@ -1,6 +1,8 @@
 extends Node
 
+const EDIT_LIST_DIALOG := preload("res://scenes/EditListDialog.tscn")
 enum DialogMode { CREATE_LIST, EDIT_LIST, EDIT_BOARD }
+
 var popup
 
 func create_single_error_popup(message : String, focus_after_close : Control, parent : Node):
@@ -29,3 +31,21 @@ func create_delete_confirm_popup(parent : Node, confirm_target : Object, binds :
 	
 	yield(dialog, "popup_hide")
 	dialog.queue_free()
+
+func create_edit_title_dialog(mode, board, list = null):
+	var overlay = ColorRect.new()
+	overlay.color = Color(0, 0, 0, 0.5)
+	overlay.set_anchors_and_margins_preset(Control.PRESET_WIDE, Control.PRESET_MODE_KEEP_SIZE)
+	get_parent().add_child(overlay)
+	
+	var dialog = EDIT_LIST_DIALOG.instance()
+	overlay.add_child(dialog)	
+	dialog.set_board(board)
+	dialog.set_mode(mode)
+	
+	if list: dialog.set_list(list)
+	dialog.popup()
+	
+	yield(dialog, "popup_hide")
+	dialog.queue_free()
+	overlay.queue_free()
