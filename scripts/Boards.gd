c4ba76d1c17ce45a38b7768daab677910b6b84eb
Alfred Reinold Baudisch <alfred.r.baudisch@gmail.com>
2020-06-13 22:06:57 +0200

    Removed the create board from the public boards section; renamed public to shared boards

--- scripts/Boards.gd
+++ scripts/Boards.gd
@@ -4,7 +4,6 @@ onready var personal_boards_container := $ScrollContainer/MarginContainer/Center
 onready var public_boards_container := $ScrollContainer/MarginContainer/CenterContainer/VBoxContainer/PublicBoardsContainer
 
 onready var create_personal_board_button := $ScrollContainer/MarginContainer/CenterContainer/VBoxContainer/PersonalBoardsContainer/CreateBoard
-onready var create_public_board_button := $ScrollContainer/MarginContainer/CenterContainer/VBoxContainer/PublicBoardsContainer/CreateBoard
 
 const BOARD_CARD := preload("res://scenes/BoardCard.tscn")
 
@@ -14,12 +13,12 @@ func _ready():
 
 func _refresh_boards():
 	Utils.clear_children(personal_boards_container, [create_personal_board_button])
-	Utils.clear_children(public_boards_container, [create_public_board_button])
+	Utils.clear_children(public_boards_container)
 	
 	for board in DataRepository.boards_by_id.values():
 		var board_card = BOARD_CARD.instance()	
 		
-		if not board.is_public:
+		if board.members.size() == 0:
 			personal_boards_container.add_child(board_card)
 		else:
 			public_boards_container.add_child(board_card)	
@@ -28,7 +27,6 @@ func _refresh_boards():
 		board_card.connect("pressed", self, "_on_board_card_pressed", [board])
 
 	_make_button_last_item(personal_boards_container, create_personal_board_button)
-	_make_button_last_item(public_boards_container, create_public_board_button)
 
 func _go_to_board(board):
 	DataRepository.set_active_board(board)
