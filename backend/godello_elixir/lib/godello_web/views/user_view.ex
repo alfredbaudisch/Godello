6eae907824a5402e723a5bfbd1e319247bd5fc67
Alfred Reinold Baudisch <alfred.r.baudisch@gmail.com>
2020-06-18 14:23:52 +0200

    User controller, with login implemented

---
+++ backend/godello_elixir/lib/godello_web/views/user_view.ex
@@ -0,0 +1,15 @@
+defmodule GodelloWeb.UserView do
+  use GodelloWeb, :view
+
+  def render("logged_on.json", %{user: user, token: token}) do
+    %{user: render_one(user, __MODULE__, "user.json"), token: token}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, __MODULE__, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{id: user.id, first_name: user.first_name, last_name: user.last_name, email: user.email}
+  end
+end
