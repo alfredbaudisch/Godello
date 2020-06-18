1da6ebc8751626d03841cb3bfb4354c9c5dbc8de
Alfred Reinold Baudisch <alfred.r.baudisch@gmail.com>
2020-06-18 14:24:12 +0200

    Test login; test user creation

---
+++ backend/godello_elixir/test/godello/accounts_test.exs
@@ -0,0 +1,55 @@
+defmodule Godello.AccountsTest do
+  use Godello.DataCase
+
+  @moduletag :accounts
+
+  alias Godello.Accounts
+  alias Godello.Accounts.User
+
+  @valid_user %{
+    first_name: "Koda",
+    last_name: "Dachshund",
+    email: "koda@thedachshund.com",
+    password: "abc123",
+    password_confirmation: "abc123"
+  }
+
+  describe "users" do
+    def user_fixture(attrs \\ %{}) do
+      {:ok, user} =
+        attrs
+        |> Enum.into(@valid_user)
+        |> Accounts.create_user()
+
+      user
+    end
+
+    test "create user" do
+      assert {:ok, %User{} = user} = Accounts.create_user(@valid_user)
+      assert user.email == @valid_user.email
+      refute user.password == user.password_hash
+    end
+
+    test "create invalid user" do
+      assert {:error, changeset} =
+               Accounts.create_user(@valid_user |> Map.put(:email, "no_email"))
+
+      assert "has invalid format" in errors_on(changeset).email
+
+      assert {:error, changeset} =
+               Accounts.create_user(
+                 @valid_user
+                 |> Map.put(:password_confirmation, "random stuff")
+               )
+
+      assert "does not match confirmation" in errors_on(changeset).password_confirmation
+    end
+
+    test "email uniqueness" do
+      user = user_fixture()
+
+      assert {:error, changeset} = Accounts.create_user(@valid_user)
+      assert "has already been taken" in errors_on(changeset).email
+    end
+  end
+end
---
+++ backend/godello_elixir/test/godello_web/controllers/user_controller_test.exs
@@ -0,0 +1,67 @@
+defmodule GodelloWeb.UserControllerTest do
+  use GodelloWeb.ConnCase
+
+  @moduletag :accounts
+
+  alias Godello.Accounts
+  alias Godello.Accounts.User
+
+  @create_attrs %{
+    first_name: "Koda",
+    last_name: "Dachshund",
+    email: "koda@thedachshund.com",
+    password: "abc123",
+    password_confirmation: "abc123"
+  }
+
+  def fixture(:user) do
+    {:ok, user} = Accounts.create_user(@create_attrs)
+    user
+  end
+
+  defp create_user(_) do
+    user = fixture(:user)
+    %{user: user}
+  end
+
+  setup %{conn: conn} do
+    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  end
+
+  describe "authentication" do
+    setup [:create_user]
+
+    test "login", %{conn: conn, user: user} do
+      conn =
+        post(conn, Routes.user_path(conn, :login),
+          email: @create_attrs.email,
+          password: @create_attrs.password
+        )
+
+      %{"user" => user, "token" => token} = json_response(conn, 200)
+      assert is_nil(user["password_hash"])
+      assert user["email"] == @create_attrs.email
+      refute is_nil(token)
+    end
+
+    test "login with invalid password", %{conn: conn, user: user} do
+      conn =
+        post(conn, Routes.user_path(conn, :login),
+          email: @create_attrs.email,
+          password: "12234234"
+        )
+
+      assert json_response(conn, 422)["errors"]["detail"] == "invalid_credentials"
+    end
+
+    test "login with inexistent email", %{conn: conn, user: user} do
+      conn =
+        post(conn, Routes.user_path(conn, :login),
+          email: "hey@koda.com",
+          password: @create_attrs.password
+        )
+
+      assert json_response(conn, 422)["errors"]["detail"] == "invalid_credentials"
+    end
+  end
+end
