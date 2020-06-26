2e8fd0fec5cc0d27750fd69fc99af389468bcc9b
Alfred Reinold Baudisch <alfred.r.baudisch@gmail.com>
2020-06-26 16:55:00 +0200

    Positioning of Lists and Cards now use the same base functions, since the logic is the same

--- backend/godello_elixir/lib/godello/kanban/positioning.ex
+++ backend/godello_elixir/lib/godello/kanban/positioning.ex
@@ -12,56 +12,30 @@ defmodule Godello.Kanban.Positioning do
   #
 
   def list_positions(board_id) do
-    positions =
-      from(item in List,
-        select: %{"id" => item.id, "position" => item.position},
-        where: item.board_id == ^board_id,
-        order_by: [asc: item.position]
-      )
-      |> Repo.all()
-
     %{
       "board" => %{
         "id" => board_id,
-        "lists" => positions
+        "lists" => positions(list_query(), board_id: board_id)
       }
     }
   end
 
   def recalculate_list_positions(board_id, list_id, starting_position) do
-    from(s in Card,
-      where:
-        s.position >= ^starting_position and
-          s.id != ^list_id and s.board_id == ^board_id
-    )
-    |> Repo.update_all(inc: [position: 1])
+    recalculate_positions(list_query(), [board_id: board_id], list_id, starting_position)
   end
 
-  def recalculate_list_positions_after_update(board_id, list_id, previous_position, new_position)
-      when new_position > previous_position do
-    from(s in Card,
-      where:
-        s.position <= ^new_position and s.position > ^previous_position and
-          s.id != ^list_id and s.board_id == ^board_id
+  def recalculate_list_positions_after_update(board_id, list_id, previous_position, new_position) do
+    recalculate_positions_after_update(
+      list_query(),
+      [board_id: board_id],
+      list_id,
+      previous_position,
+      new_position
     )
-    |> Repo.update_all(inc: [position: -1])
-  end
-
-  def recalculate_list_positions_after_update(board_id, list_id, previous_position, new_position)
-      when new_position < previous_position do
-    from(s in Card,
-      where:
-        s.position >= ^new_position and s.position < ^previous_position and
-          s.id != ^list_id and s.board_id == ^board_id
-    )
-    |> Repo.update_all(inc: [position: 1])
   end
 
   def recalculate_list_positions_after_delete(board_id, previous_position) do
-    from(s in Card,
-      where: s.position > ^previous_position and s.board_id == ^board_id
-    )
-    |> Repo.update_all(inc: [position: -1])
+    recalculate_positions_after_delete(list_query(), [board_id: board_id], previous_position)
   end
 
   #
@@ -69,55 +43,99 @@ defmodule Godello.Kanban.Positioning do
   #
 
   def card_positions(list_id) do
-    positions =
-      from(item in Card,
-        select: %{"id" => item.id, "position" => item.position},
-        where: item.list_id == ^list_id,
-        order_by: [asc: item.position]
-      )
-      |> Repo.all()
-
     %{
       "list" => %{
         "id" => list_id,
-        "cards" => positions
+        "cards" => positions(card_query(), list_id: list_id)
       }
     }
   end
 
   def recalculate_card_positions(list_id, card_id, starting_position) do
-    from(s in Card,
+    recalculate_positions(card_query(), [list_id: list_id], card_id, starting_position)
+  end
+
+  def recalculate_card_positions_after_update(list_id, card_id, previous_position, new_position) do
+    recalculate_positions_after_update(
+      card_query(),
+      [list_id: list_id],
+      card_id,
+      previous_position,
+      new_position
+    )
+  end
+
+  def recalculate_card_positions_after_delete(list_id, previous_position) do
+    recalculate_positions_after_delete(card_query(), [list_id: list_id], previous_position)
+  end
+
+  #
+  # Algorithms / Queries
+  #
+
+  defp list_query do
+    from(i in List)
+  end
+
+  defp card_query do
+    from(i in Card)
+  end
+
+  defp positions(base_query, parent_condition) do
+    from(item in base_query,
+      select: %{"id" => item.id, "position" => item.position},
+      where: ^parent_condition,
+      order_by: [asc: item.position]
+    )
+    |> Repo.all()
+  end
+
+  defp recalculate_positions(base_query, parent_condition, item_id, starting_position) do
+    from(item in base_query,
       where:
-        s.position >= ^starting_position and
-          s.id != ^card_id and s.list_id == ^list_id
+        item.position >= ^starting_position and
+          item.id != ^item_id,
+      where: ^parent_condition
     )
     |> Repo.update_all(inc: [position: 1])
   end
 
-  def recalculate_card_positions_after_update(list_id, card_id, previous_position, new_position)
-      when new_position > previous_position do
-    from(s in Card,
+  defp recalculate_positions_after_update(
+         base_query,
+         parent_condition,
+         item_id,
+         previous_position,
+         new_position
+       )
+       when new_position > previous_position do
+    from(item in base_query,
       where:
-        s.position <= ^new_position and s.position > ^previous_position and
-          s.id != ^card_id and s.list_id == ^list_id
+        item.position <= ^new_position and item.position > ^previous_position and
+          item.id != ^item_id,
+      where: ^parent_condition
     )
     |> Repo.update_all(inc: [position: -1])
   end
 
-  def recalculate_card_positions_after_update(list_id, card_id, previous_position, new_position)
-      when new_position < previous_position do
-    from(s in Card,
+  defp recalculate_positions_after_update(
+         base_query,
+         parent_condition,
+         item_id,
+         previous_position,
+         new_position
+       )
+       when new_position < previous_position do
+    from(item in base_query,
       where:
-        s.position >= ^new_position and s.position < ^previous_position and
-          s.id != ^card_id and s.list_id == ^list_id
+        item.position >= ^new_position and item.position < ^previous_position and
+          item.id != ^item_id,
+      where: ^parent_condition
     )
     |> Repo.update_all(inc: [position: 1])
   end
 
-  def recalculate_card_positions_after_delete(list_id, previous_position) do
-    from(s in Card,
-      where: s.position > ^previous_position and s.list_id == ^list_id
-    )
+  defp recalculate_positions_after_delete(base_query, parent_condition, previous_position) do
+    from(item in base_query, where: item.position > ^previous_position, where: ^parent_condition)
     |> Repo.update_all(inc: [position: -1])
   end
 end
