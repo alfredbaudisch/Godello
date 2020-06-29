fcc2cceafe1832feef3d4e294725266a94cba2d0
Alfred Reinold Baudisch <alfred.r.baudisch@gmail.com>
2020-06-29 21:43:01 +0200

    confirm_password -> password_confirmation

--- scripts/LoginRegister.gd
+++ scripts/LoginRegister.gd
@@ -13,7 +13,7 @@ onready var first_name_input := $"/root/LoginRegister".find_node("FirstName")
 onready var last_name_input := $"/root/LoginRegister".find_node("LastName")
 onready var email_input := $"/root/LoginRegister".find_node("Email")
 onready var password_input := $"/root/LoginRegister".find_node("Password")
-onready var confirm_password_input := $"/root/LoginRegister".find_node("ConfirmPassword")
+onready var password_confirmation_input := $"/root/LoginRegister".find_node("PasswordConfirmation")
 onready var login_email_input := $"/root/LoginRegister".find_node("SignInEmail")
 onready var login_password_input := $"/root/LoginRegister".find_node("SignInPassword")
 
@@ -73,7 +73,7 @@ func _sign_up():
 	var last_name = Utils.clean_input_text(last_name_input.get_text())
 	var email = Utils.clean_input_text(email_input.get_text())
 	var password = password_input.get_text()
-	var confirm_password = confirm_password_input.get_text()
+	var password_confirmation = password_confirmation_input.get_text()
 	
 	# Basic validation
 	if (
@@ -82,17 +82,24 @@ func _sign_up():
 		and Utils.validate_not_empty_text(email, email_input.get_placeholder(), email_input, self)
 		and Utils.validate_email_field(email_input, self)
 		and Utils.validate_not_empty_text(password, password_input.get_placeholder(), password_input, self)
-		and Utils.validate_not_empty_text(confirm_password, confirm_password_input.get_placeholder(), confirm_password_input, self)
+		and Utils.validate_not_empty_text(password_confirmation, password_confirmation_input.get_placeholder(), password_confirmation_input, self)
 	):
 		# Password validation
 		if password.length() < PASSWORD_MIN_LENGTH:
 			SceneUtils.create_single_error_popup("Password must contain at least " + str(PASSWORD_MIN_LENGTH) + " characters.", password_input, self)			
-		elif password != confirm_password:
-			SceneUtils.create_single_error_popup("Confirm password does not match Password.", confirm_password_input, self)			
+		elif password != password_confirmation:
+			SceneUtils.create_single_error_popup("Confirm password does not match Password.", password_confirmation_input, self)			
 		
 		# All good, sign up
 		else:
-			print("TODO: SIGN UP!")
-			var user = UserModel.new(UUID.v4(), first_name, last_name, email)			
-			DataRepository.set_active_user(user)
-			SceneUtils.go_to_main_route()
+			Backend.sign_up({
+				first_name = first_name,
+				last_name = last_name,
+				email = email,
+				password = password,
+				password_confirmation = password_confirmation
+			})
+			
+#			var user = UserModel.new(UUID.v4(), first_name, last_name, email)			
+#			DataRepository.set_active_user(user)
+#			SceneUtils.go_to_main_route()
