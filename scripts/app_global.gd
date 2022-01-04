bb31df9a61bc1b6a4243e40efc81a492e2d16404
CptFubar <30928828+CptFubar@users.noreply.github.com>
2022-01-04 00:24:28 +0000

    Moved the backend local singelton from being autoloaded to being dynamically loaded through the app_global singleton.
    This forces to only have one backend solution loaded at all times.

--- scripts/app_global.gd
+++ scripts/app_global.gd
@@ -2,17 +2,20 @@ extends Node
 
 
 enum Storage {LOCAL,ELIXIR}
+
 var backend : int = Storage.LOCAL setget _set_backend	# Default setting to local
 var local_repository : String = "user://repository"
 var executable_path : String
 var config_file_path = "user://config.cfg"
 var local_owner = UserModel.new("1","Local","User")		# temporary user for local backend might add the feature of multiple local users.
+var local_backend = preload("res://scripts/local_storage.gd")
 
 
 func _ready() -> void:
 	OS.min_window_size  = Vector2(600.0,400.0)
 	executable_path = OS.get_executable_path()
 	load_config()
+	_set_backend(backend)
 
 
 func load_config() -> void:
@@ -41,3 +44,13 @@ func save_config() -> void:
 func _set_backend(value) -> void:
 	backend = value
 	save_config()
+	# assuming there will be only 1 backend loaded at times the frst child (Backend module) will be removed
+	if get_child_count() > 0:
+		get_child(0).queue_free()
+
+	match value:
+		Storage.LOCAL:
+			add_child(local_backend.new())
+		Storage.ELIXIR:
+			pass
+
